99d6ceeda6388e486ba08a9642cb5c9221d98742
KM <siukwongman@gmail.com>
2023-01-03 18:22:23 +0800

    Updated Tomorrow's Weather Part.

--- js/tomorrowWeather.js
+++ js/tomorrowWeather.js
@@ -1,1 +1,66 @@
-// 請KM負責這個文件
+const model = {
+    init: function(){
+        async function getWeatherData(url){
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        };
+        
+        const weatherDataTempPromise = getWeatherData("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-25142137-EFE4-4F9E-9B46-D41BF5BD73D5");
+        const weatherDataRainfallPromise = getWeatherData("https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-25142137-EFE4-4F9E-9B46-D41BF5BD73D5");
+        
+        Promise.all([weatherDataTempPromise, weatherDataRainfallPromise])
+        .then(([dataTemp, dataRainfall]) => {
+            view.render(dataTemp, dataRainfall);
+        });
+    }
+};
+
+const view = {
+    render: function(dataTemp, dataRainfall){
+        const nightWeatherList = {
+            "陰時多雲": "/img/night_cloudy_shade.png",
+            "陰天": "/img/night_overcast.png",
+            "多雲": "/img/night_cloudy.png",
+            "晴時多雲": "/img/night_cloudy_sunny_period.png",
+            "陰短暫雨": "/img/night_short_rain_shade.png",
+            "多雲時陰": "/img/night_most_cloudy_shade.png",
+            "陰有雨": "/img/night_rain_shade.png",
+            "多雲時晴": "/img/night_cloudy_sunny.png"
+        };
+        
+        const dayWeatherList = {
+            "陰時多雲": "/img/day_cloudy_shade.png",
+            "陰天": "/img/day_overcast.png",
+            "多雲": "/img/day_cloudy.png",
+            "晴時多雲": "/img/day_cloudy_sunny_period.png",
+            "陰短暫雨": "/img/day_short_rain_shade.png",
+            "多雲時陰": "/img/day_most_cloudy_shade.png",
+            "陰有雨": "/img/day_rain_shade.png",
+            "多雲時晴": "/img/day_cloudy_sunny.png"
+        };
+
+        // Display the content
+        document.querySelector("#todayNightWeather").style.opacity = "1";
+        document.querySelector("#tomorrowDayWeather").style.opacity = "1";
+    
+        // Weather Icon
+        document.querySelector("#todayNightWeatherIcon").src = nightWeatherList[dataTemp.records.location[0].weatherElement[0].time[1].parameter.parameterName];
+        document.querySelector("#todayDayWeatherIcon").src = dayWeatherList[dataTemp.records.location[0].weatherElement[0].time[2].parameter.parameterName];
+        
+        // Temperature
+        document.querySelector("#todayNightWeatherTemp").textContent = dataTemp.records.location[0].weatherElement[2].time[1].parameter.parameterName + " °C - " + dataTemp.records.location[0].weatherElement[4].time[1].parameter.parameterName + " °C";
+        document.querySelector("#tomorrowDayWeatherTemp").textContent = dataTemp.records.location[0].weatherElement[2].time[2].parameter.parameterName + " °C - " + dataTemp.records.location[0].weatherElement[4].time[2].parameter.parameterName + " °C";
+    
+        // Rainfall
+        document.querySelector("#todayNightRainfallRate").textContent = dataRainfall.records.location[4].weatherElement[6].elementValue + " mm";
+        document.querySelector("#tomorrowDayRainFallRate").textContent = dataRainfall.records.location[4].weatherElement[6].elementValue + " mm";
+    }
+};
+
+const controller = {
+    init: function(){
+        model.init();
+    }
+};
+controller.init();
