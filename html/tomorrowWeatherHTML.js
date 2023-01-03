d84d00545d5cbc1b96c1aac8e264ca37d096b814
KM <siukwongman@gmail.com>
2023-01-03 19:19:29 +0800

    Updated All Variable Names from Today Night to Tomorrow Day.

--- html/tomorrowWeatherHTML.js
+++ html/tomorrowWeatherHTML.js
@@ -4,33 +4,33 @@ class TomorrowWeather extends HTMLElement{
         this.innerHTML = `
             <div class="tomorrow-weather-container">
 
-                <div id="todayNightWeather" class="today-night-weather border">
-                    <div class="today-night-weather-title">今晚明晨</div>
-                    <div class="today-night-weather-period">18:00 ~ 06:00</div>
-                    <div class="today-night-weather-icon-frame">
-                        <img id="todayNightWeatherIcon" class="today-night-weather-icon" src="">
-                    </div>
-                    <div id="todayNightWeatherTemp" class="today-night-weather-temp"><!--今晚明晨溫度DIV-->></div>
-                    <div class="today-night-rainfall">
-                        <div class="today-night-rainfall-icon">
-                            <img src="/img/icon_umbrella.png" src="">
-                        </div>
-                        <div id="todayNightRainfallRate" class="today-night-rainfall-rate"><!--今晚明晨降雨量DIV--></div>
-                    </div>
-                </div>
-
-                <div id="tomorrowDayWeather" class="tomorrow-day-weather">
+                <div id="tomorrowDayWeather" class="tomorrow-day-weather border">
                     <div class="tomorrow-day-weather-title">明日白天</div>
                     <div class="tomorrow-day-weather-period">06:00 ~ 18:00</div>
                     <div class="tomorrow-day-weather-icon-frame">
-                        <img id="todayDayWeatherIcon" class="tomorrow-day-weather-icon" src="">
+                        <img id="tomorrowDayWeatherIcon" class="tomorrow-day-weather-icon" src="">
                     </div>
-                    <div id="tomorrowDayWeatherTemp" class="tomorrow-day-weather-temp"><!--明日白天溫度DIV--></div>
+                    <div id="tomorrowDayWeatherTemp" class="tomorrow-day-weather-temp"><!--明日白天溫度DIV-->></div>
                     <div class="tomorrow-day-rainfall">
                         <div class="tomorrow-day-rainfall-icon">
+                            <img src="/img/icon_umbrella.png" src="">
+                        </div>
+                        <div id="tomorrowDayRainfallRate" class="tomorrow-day-rainfall-rate"><!--明日白天降雨量DIV--></div>
+                    </div>
+                </div>
+
+                <div id="tomorrowNightWeather" class="tomorrow-night-weather">
+                    <div class="tomorrow-night-weather-title">明日晚上</div>
+                    <div class="tomorrow-night-weather-period">18:00 ~ 06:00</div>
+                    <div class="tomorrow-night-weather-icon-frame">
+                        <img id="tomorrowNightWeatherIcon" class="tomorrow-night-weather-icon" src="">
+                    </div>
+                    <div id="tomorrowNightWeatherTemp" class="tomorrow-night-weather-temp"><!--明日晚上溫度DIV--></div>
+                    <div class="tomorrow-night-rainfall">
+                        <div class="tomorrow-night-rainfall-icon">
                             <img src="/img/icon_umbrella.png">
                         </div>
-                        <div id="tomorrowDayRainFallRate" class="tomorrow-day-rainfall-rate"><!--今明日白天降雨量DIV--></div>
+                        <div id="tomorrowNightRainFallRate" class="tomorrow-night-rainfall-rate"><!--明日晚上降雨量DIV--></div>
                     </div>
                 </div>
 
