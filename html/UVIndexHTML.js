38b4dc760c63c95d244212d8c4a65713873361f3
Sleepingisimportant <43533116+Sleepingisimportant@users.noreply.github.com>
2023-01-04 21:41:10 +0100

    UVIndex Widget

--- html/UVIndexHTML.js
+++ html/UVIndexHTML.js
@@ -1,13 +1,33 @@
 // 請俐蓉負責這個文件
 
-class UVIndex extends HTMLElement{
 
-    connectedCallback(){
+class UVIndex extends HTMLElement {
+
+    connectedCallback() {
         this.innerHTML = `
-            <div>UV Index</div>
+        <div id="div-uv-index">
+        <p>UV紫外線指數</p>
+
+            <div id="dropdown-station-name">
+            <label for="station-names">選擇測站：</label>
+<select name="station-names" id="select-station-names">
+<option value="">請選擇</option>
+</select>
+</div>
+
+<div id="icon-and-bar">
+
+<div id="uv-index-emoji">&#9728</div>
+
+            <div id="uv-index-bar">
+            <progress id="uv-index-progress-bar" max="12" value="0">  </progress>
+            </div>
+            </div>
+            <p id="uv-index-datatime"></p>
+    </div>
         `;
     };
-    
+
 };
 
 customElements.define("uv-index", UVIndex);
--- js/UVIndex.js
+++ js/UVIndex.js
@@ -1,1 +1,100 @@
-// 請俐蓉負責這個文件
+// // 請俐蓉負責這個文件
+const CWB_API_KEY = "CWB-B37454BB-118F-4B9D-A0E7-99FE16FF324A";
+
+const model = {
+    constructor: function () {
+        async function getData(url) {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        };
+
+        const ultraVioletData = getData("https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0005-001?Authorization=" + CWB_API_KEY);
+        const stationData = getData("https://opendata.cwb.gov.tw/api/v1/rest/datastore/C-B0074-001?Authorization=" + CWB_API_KEY);
+
+        Promise.all([ultraVioletData, stationData])
+            .then(([ultraVioletData, stationData]) => {
+                view.render(ultraVioletData, stationData);
+            });
+    }
+};
+
+
+const view = {
+    render: function (ultraVioletData, stationData) {
+
+        let stationList = [];
+        let master = [];
+
+
+        console.log(ultraVioletData)
+
+        let stations = stationData['records']['data']['stationStatus']['station']
+        for (let i = 0; i < stations.length; i++) {
+            stationList.push({ 'stationID': stations[i]['StationID'], 'stationName': stations[i]['StationName'] });
+        }
+        console.log(stationList)
+
+        ultraVioletValue = ultraVioletData['records']['weatherElement']['location'];
+        ultraVioletValue.forEach((element) => {
+            for (let j = 0; j < stationList.length; j++) {
+                if (element.locationCode == stationList[j]['stationID']) {
+
+                    master.push({ 'stationID': stationList[j]['stationID'], 'stationName': stationList[j]['stationName'], 'uvValue': element.value });
+                    return
+                }
+            }
+        });
+
+        console.log(stationList)
+        console.log(master)
+
+
+        master.forEach((element) => {
+            insertHtml = `<option value="` + element.stationName + `">` + element.stationName + `</option>`;
+            dropdownStationName = document.querySelector("#select-station-names");
+            dropdownStationName.insertAdjacentHTML("afterbegin", insertHtml);
+        });
+
+        let option = document.getElementById("select-station-names")
+        option.addEventListener("change", async function () {
+            stationName = this.value;
+
+            master.forEach((e) => {
+                if (e.stationName == stationName) {
+                    uvValue = e.uvValue;
+
+                    let uvIndexProgressBar = document.querySelector("#uv-index-progress-bar");
+                    uvIndexProgressBar.remove();
+
+                    let uvIndexBar = document.querySelector("#uv-index-bar");
+                    insertHtml = `<progress id="uv-index-progress-bar" max="12" value="` + uvValue + `">  </progress>`
+                    uvIndexBar.insertAdjacentHTML("afterbegin", insertHtml)
+                    uvIndexEmoji = document.querySelector("#uv-index-emoji");
+
+                    if (uvValue >= 0 && uvValue < 3) {
+                        uvIndexEmoji.style.color = "#00F500";
+                    } else if (uvValue >= 3 && uvValue < 7) {
+                        uvIndexEmoji.style.color = "yellow";
+                    } else if (uvValue >= 7 && uvValue < 10) {
+                        uvIndexEmoji.style.color = "red";
+                    } else {
+                        uvIndexEmoji.style.color = "purple";
+
+                    }
+                }
+            })
+
+        });
+
+
+
+    }
+}
+
+const control = {
+    constructor: function () {
+        model.constructor();
+    }
+};
+control.constructor();
